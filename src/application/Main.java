777bb53b4500820601d8dbd5097e88472edb36e2
Matheus Passos <matheus.senai2016@gmail.com>
2025-04-11 01:14:23 -0300

    Implementação da interface

---
+++ src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import entities.Product;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<Product>();
+
+
+
+        list.add(new Product("tv", 900.00));
+        list.add(new Product("hd", 700.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Celular", 900.00));
+
+        list.forEach(new PriceUpdate());
+
+        list.forEach(System.out::println);
+    }
+}
