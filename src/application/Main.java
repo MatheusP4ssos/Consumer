37eb3b841c5362d05be99c62077a30444f5ceab3
Matheus Passos <matheus.senai2016@gmail.com>
2025-04-11 01:25:57 -0300

    Expressão Lambda declarada

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import util.PriceUpdate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,7 +21,13 @@ public class Main {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Celular", 900.00));
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        double factor = 1.1;
+
+        Consumer<Product> cons = p -> {
+            p.setPrice(p.getPrice() * factor);
+        };
+
+        list.forEach(cons);
 
         list.forEach(System.out::println);
     }
